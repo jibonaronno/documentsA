28319106a0fa7b9f70d45358cf4f5809db55ff88
PDACLOUD\jibon <tebunia@sanion.com>
2024-09-04 18:00:19 +0900

    4 Sept Office 001

---
+++ uprojects/Laurent ESP Mesh/CrowPanel-ESP32-Display-Course-File/CrowPanel_ESP32_Tutorial/Codes/CrowPanel_Lesson6/CrowPanel_Lesson6.ino
@@ -0,0 +1,326 @@
+/**************************CrowPanel ESP32 HMI Display Example Code************************
+Version     :	1.0
+Suitable for:	CrowPanel ESP32 HMI Display
+Product link:	https://www.elecrow.com/esp32-display-series-hmi-touch-screen.html
+Code	  link:	https://github.com/Elecrow-RD/CrowPanel-ESP32-Display-Course-File
+Lesson	link:	https://www.youtube.com/watch?v=WHfPH-Kr9XU
+Description	:	The code is currently available based on the course on YouTube, 
+				        if you have any questions, please refer to the course video: Introduction 
+				        to ask questions or feedback.
+**************************************************************/
+
+
+#include <Wire.h>
+#include <SPI.h>
+#include <painlessMesh.h>
+#include <ArduinoJson.h>
+
+/**************************LVGL and UI************************
+if you want to use the LVGL demo. you need to include <demos/lv_demos.h> and <examples/lv_examples.h>. 
+if not, please do not include it. It will waste your Flash space.
+**************************************************************/
+#include <lvgl.h>
+#include "ui.h"
+// #include <demos/lv_demos.h>
+// #include <examples/lv_examples.h>
+/**************************LVGL and UI END************************/
+
+/*******************************************************************************
+   Config the display panel and touch panel in gfx_conf.h
+ ******************************************************************************/
+#include "gfx_conf.h"
+
+#include <cppQueue.h>
+
+#define	IMPLEMENTATION	LIFO
+
+#define LED 10
+
+#define   BLINK_PERIOD    3000 // milliseconds until cycle repeat
+#define   BLINK_DURATION  100  // milliseconds LED is on for
+
+#define   MESH_SSID       "mesh"
+#define   MESH_PASSWORD   "12345678"
+#define   MESH_PORT       5555
+
+// Prototypes
+void sendMessage(); 
+void receivedCallback(uint32_t from, String & msg);
+void newConnectionCallback(uint32_t nodeId);
+void changedConnectionCallback(); 
+void nodeTimeAdjustedCallback(int32_t offset); 
+void delayReceivedCallback(uint32_t from, int32_t delay);
+
+Scheduler     userScheduler; // to control your personal task
+painlessMesh  mesh;
+
+JsonDocument doc;
+
+bool calc_delay = false;
+SimpleList<uint32_t> nodes;
+
+void lvgl_regular_function();
+Task taskLvgl(5, TASK_FOREVER, &lvgl_regular_function);
+
+void sendMessage() ; // Prototype
+Task taskSendMessage( TASK_SECOND * 1, TASK_FOREVER, &sendMessage ); // start with a one second interval
+
+// Task to blink the number of nodes
+Task blinkNoNodes;
+bool onFlag = false;
+
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t disp_draw_buf1[screenWidth * screenHeight / 8];
+static lv_color_t disp_draw_buf2[screenWidth * screenHeight / 8];
+static lv_disp_drv_t disp_drv;
+
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+   uint32_t w = ( area->x2 - area->x1 + 1 );
+   uint32_t h = ( area->y2 - area->y1 + 1 );
+
+   tft.pushImageDMA(area->x1, area->y1, w, h,(lgfx::rgb565_t*)&color_p->full);
+
+   lv_disp_flush_ready( disp );
+
+}
+
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+   uint16_t touchX, touchY;
+   bool touched = tft.getTouch( &touchX, &touchY);
+   if( !touched )
+   {
+      data->state = LV_INDEV_STATE_REL;
+   }
+   else
+   {
+      data->state = LV_INDEV_STATE_PR;
+
+      /*Set the coordinates*/
+      data->point.x = touchX;
+      data->point.y = touchY;
+
+      Serial.print( "Data x " );
+      Serial.println( touchX );
+
+      Serial.print( "Data y " );
+      Serial.println( touchY );
+   }
+}
+
+extern lv_obj_t * ui_Bar1;
+extern lv_obj_t * ui_Bar2;
+int progress_01 = 10;
+int progress_02 = 10;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("LVGL Widgets Demo");
+
+  //GPIO init
+#if defined (CrowPanel_50) || defined (CrowPanel_70)
+  pinMode(38, OUTPUT);
+  digitalWrite(38, LOW);
+  pinMode(17, OUTPUT);
+  digitalWrite(17, LOW);
+  pinMode(18, OUTPUT);
+  digitalWrite(18, LOW);
+  pinMode(42, OUTPUT);
+  digitalWrite(42, LOW);
+#elif defined (CrowPanel_43)
+  pinMode(20, OUTPUT);
+  digitalWrite(20, LOW);
+  pinMode(19, OUTPUT);
+  digitalWrite(19, LOW);
+  pinMode(35, OUTPUT);
+  digitalWrite(35, LOW);
+  pinMode(38, OUTPUT);
+  digitalWrite(38, LOW);
+  pinMode(0, OUTPUT);//TOUCH-CS
+#endif
+
+  //Display Prepare
+  tft.begin();
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextSize(2);
+  delay(200);
+
+  lv_init();
+
+  delay(100);
+
+  lv_disp_draw_buf_init(&draw_buf, disp_draw_buf1, disp_draw_buf2, screenWidth * screenHeight/8);
+  /* Initialize the display */
+  lv_disp_drv_init(&disp_drv);
+  /* Change the following line to your display resolution */
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.full_refresh = 1;
+  disp_drv.draw_buf = &draw_buf;
+  lv_disp_drv_register(&disp_drv);
+
+  /* Initialize the (dummy) input device driver */
+  static lv_indev_drv_t indev_drv;
+  lv_indev_drv_init(&indev_drv);
+  indev_drv.type = LV_INDEV_TYPE_POINTER;
+  indev_drv.read_cb = my_touchpad_read;
+  lv_indev_drv_register(&indev_drv);
+
+  tft.fillScreen(TFT_BLACK);
+
+  //please do not use LVGL Demo and UI export from Squareline Studio in the same time.
+  // lv_demo_widgets();    // LVGL demo
+  ui_init();
+
+
+
+
+  mesh.setDebugMsgTypes(ERROR | DEBUG);  // set before init() so that you can see error messages
+
+  mesh.init(MESH_SSID, MESH_PASSWORD, &userScheduler, MESH_PORT);
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+  mesh.onNodeDelayReceived(&delayReceivedCallback);
+
+  userScheduler.addTask( taskSendMessage );
+  taskSendMessage.enable();
+
+  userScheduler.addTask(taskLvgl);
+  taskLvgl.enable();
+
+  /*
+  blinkNoNodes.set(BLINK_PERIOD, (mesh.getNodeList().size() + 1) * 2, []() {
+      // If on, switch off, else switch on
+      if (onFlag)
+        onFlag = false;
+      else
+        onFlag = true;
+      blinkNoNodes.delay(BLINK_DURATION);
+
+      if (blinkNoNodes.isLastIteration()) {
+        // Finished blinking. Reset task for next run 
+        // blink number of nodes (including this node) times
+        blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
+        // Calculate delay based on current mesh time and BLINK_PERIOD
+        // This results in blinks between nodes being synced
+        blinkNoNodes.enableDelayed(BLINK_PERIOD - 
+            (mesh.getNodeTime() % (BLINK_PERIOD*1000))/1000);
+      }
+  });
+  userScheduler.addTask(blinkNoNodes);
+  blinkNoNodes.enable();
+  */
+
+  Serial.println( "Setup done" );
+
+}
+
+void lvgl_regular_function()
+{
+    lv_timer_handler();
+    lv_bar_set_value(ui_Bar1, progress_01, LV_ANIM_ON);
+    lv_bar_set_value(ui_Bar2, progress_02, LV_ANIM_ON);
+}
+
+void loop()
+{
+    
+    // if(progress_01 < 100)
+    // {
+    //   progress_01++;
+    // }
+    // else
+    // {
+    //   progress_01 = 0; 
+    // }
+    mesh.update();
+    // delay(1);
+}
+
+void sendMessage() {
+  String msg = "(A)-";
+  msg += mesh.getNodeId();
+  //msg += " myFreeMemory: " + String(ESP.getFreeHeap());
+  mesh.sendBroadcast(msg);
+
+  if (calc_delay) {
+    SimpleList<uint32_t>::iterator node = nodes.begin();
+    while (node != nodes.end()) {
+      mesh.startDelayMeas(*node);
+      node++;
+    }
+    calc_delay = false;
+  }
+
+  //Serial.printf("Sending message: %s\n", msg.c_str());
+  Serial.printf(".");
+  
+  //taskSendMessage.setInterval( random(TASK_SECOND * 1, TASK_SECOND * 5));  // between 1 and 5 seconds
+  taskSendMessage.setInterval(TASK_SECOND * 10);
+}
+
+
+void receivedCallback(uint32_t from, String & msg) {
+  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+
+  deserializeJson(doc, msg.c_str());
+  const char *name = doc["name"];
+  int intense = doc["intense"];
+  if(name[0] == 'B'){
+    progress_01 = intense / 21;
+  }
+
+  if(name[0] == 'C'){
+    progress_02 = intense / 21;
+  }
+
+  Serial.printf("%s:%d\n", name, intense);
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+  // Reset blink task
+  onFlag = false;
+  //blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
+  //blinkNoNodes.enableDelayed(BLINK_PERIOD - (mesh.getNodeTime() % (BLINK_PERIOD*1000))/1000);
+ 
+  Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+  Serial.printf("--> startHere: New Connection, %s\n", mesh.subConnectionJson(true).c_str());
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+  // Reset blink task
+  onFlag = false;
+  //blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
+  //blinkNoNodes.enableDelayed(BLINK_PERIOD - (mesh.getNodeTime() % (BLINK_PERIOD*1000))/1000);
+ 
+  nodes = mesh.getNodeList();
+
+  Serial.printf("Num nodes: %d\n", nodes.size());
+  Serial.printf("Connection list:");
+
+  SimpleList<uint32_t>::iterator node = nodes.begin();
+  while (node != nodes.end()) {
+    Serial.printf(" %u", *node);
+    node++;
+  }
+  Serial.println();
+  calc_delay = true;
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
+}
+
+void delayReceivedCallback(uint32_t from, int32_t delay) {
+  Serial.printf("Delay to node %u is %d us\n", from, delay);
+}
+
