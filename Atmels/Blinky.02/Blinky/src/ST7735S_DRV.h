5aa3aaea31177f8d4dce9995cc3f2fc903de7b60
jibonaronno <tebunia@gmail.com>
2025-09-19 18:02:54 +0900

    19 Sept 25 Office

---
+++ Atmels/Blinky.02/Blinky/src/ST7735S_DRV.h
@@ -0,0 +1,16 @@
+/*
+ * ST7735S_DRV.h
+ *
+ * Created: 9/19/2025 5:00:39 PM
+ *  Author: tebun
+ */ 
+
+
+#ifndef ST7735S_DRV_H_
+#define ST7735S_DRV_H_
+
+
+
+
+
+#endif /* ST7735S_DRV_H_ */
---
+++ Atmels/Blinky.03/Blinky/src/ASF/common/boards/user_board/init.c
@@ -0,0 +1,35 @@
+/**
+ * \file
+ *
+ * \brief User board initialization template
+ *
+ */
+/*
+ * Support and FAQ: visit <a href="https://www.microchip.com/support/">Microchip Support</a>
+ */
+
+#include <asf.h>
+#include <board.h>
+#include <conf_board.h>
+
+#define PIN_USART1_TXD_FLAGS		(PIO_PERIPH_A | PIO_DEFAULT)
+#define PIN_USART1_RXD_FLAGS		(PIO_PERIPH_A | PIO_DEFAULT)
+
+#define PIN_USART1_SPI_SCK_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+#define PIN_USART1_SPI_NSS_FLAGS      (PIO_PERIPH_A | PIO_DEFAULT)
+
+void board_init(void)
+{
+	/* This function is meant to contain board-specific initialization code
+	 * for, e.g., the I/O pins. The initialization can rely on application-
+	 * specific board configuration, found in conf_board.h.
+	 */
+	gpio_configure_pin(PIO_PA22_IDX, PIN_USART1_TXD_FLAGS);
+	gpio_configure_pin(PIO_PA21_IDX, PIN_USART1_RXD_FLAGS);
+	gpio_configure_pin(PIO_PA23_IDX, PIN_USART1_SPI_SCK_FLAGS);
+	gpio_configure_pin(PIO_PA24_IDX, PIN_USART1_SPI_NSS_FLAGS);
+	
+	// gpio_configure_pin(PIO_PA23A_SCK1, PIN_USART1_SPI_SCK_FLAGS);
+	// gpio_configure_pin(PIO_PA24A_RTS1, PIN_USART1_SPI_NSS_FLAGS);
+	
+}
